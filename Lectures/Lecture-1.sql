66c0099ff0d501c0790d33abd3c0574ccb33ba0c
Suhagiya Piyush <piyush.suhagiya.ps@gmail.com>
2024-06-27 09:58:08 +0530

    27-6-24

---
+++ Lectures/Lecture-1.sql
@@ -0,0 +1,30 @@
+My SQL Querys:-
+
+=> Connect MySQL Command : 
+-- \sql
+
+=> Connect to Database Command : 
+-- \connect (username)root@localhost:3306
+
+=> Show Database Command : 
+-- show databases;
+
+=> Create New Database Command : 
+-- create database (Database Name)students;
+
+=> Show Table in Database Command : 
+-- show tables;
+
+=> Select Database Command : 
+-- use (Database Name)students;
+
+=> Delete Database Command : 
+-- drop database (Database Name)students;
+
+=> Create New Table Command: 
+-- create table (TableName)user(firstname varchar(30), lastname varchar(30), age int);
+--                          ^           ^                       ^               ^
+--            user is TableName      First Row			   Second Row		Third Row
+
+=> Discribe Table Command: 
+-- desc user; & describe user;
