5b48ca633f6da54b618c902fbca9cba8d475d16e
Suhagiya Piyush <piyush.suhagiya.ps@gmail.com>
2024-07-08 08:09:00 +0530

    8-7-24

---
+++ Lectures/Lecture-4.sql
@@ -0,0 +1,9 @@
+=> Create DB -> users
+=> Create Table -> Students -> (firestman, lastname, age, city, maths, eng, guj)
+=> Enter 10 Datas
+
+=> Querys:
+-- create database users;
+-- use users;
+-- create table students(firstname varchar(15), lastname varchar(15), age int, city varchar(20), maths int, eng int, guj int);
+-- insert into students values("Piyush", "Suhagiya", 28, "Surat", 78, 82, 96), ("Jayan", "Dudakiya", 22, "Pune", 80, 85, 92), ("Sahil", "Dudhat", 19, "Vadodara", 75, 88, 95), ("Akshay", "Goti", 23, "Surat", 85, 76, 89), ("Nikhil", "Kalsariya", 26, "Rajkot", 92, 87, 96), ("Smit", "Thummar", 20, "Surat", 65, 63, 79), ("Deep", "Lakhani", 25, "Mahesana", 77, 82, 94), ("Utsav", "Suhagiya", 19, "Ahemdabad", 70, 80, 90), ("Darshan", "Patel", 22, "Vadodara", 71, 79, 93), ("Nikunj", "Undhad", 27, "Ahemdabad", 72, 81, 95);  
